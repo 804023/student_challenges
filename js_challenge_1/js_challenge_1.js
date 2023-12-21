b15fe9517616b37d3065cbcd57e313ff8ad332cc
804023 <804023@sylvaniastudents.org>
2023-12-21 10:49:46 -0500

    worked on js_challenge_1

--- js_challenge_1/js_challenge_1.js
+++ js_challenge_1/js_challenge_1.js
@@ -1,11 +1,16 @@
 //ask user their name
-let firstName=prompt ("What is your name?");
-
-// add second function here 
+let firstName=prompt ("What is your first name?");
+let lastName = prompt("What is your last name?");
 
 // update the alert 
-alert("Hello " + firstName);
+alert("Hello " + firstName + " " + lastName);
 
 // ask user how old they are & set age rules (hint: use an if/else statement here)
-
+let age = prompt("How old are you?");
+if(age < 14) {
+    alert("You are too young to apply. You must be at least 14 years old to apply.");
+}
+else{
+    alert("Welcome!");
+}
 
